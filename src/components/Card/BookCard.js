f66f97744c22ccec3fc339216771675f11543e23
Anuj sharma <aanuj.sharma2590@gmail.com>
2021-09-27 17:01:04 +0530

    last

--- src/components/Card/BookCard.js
+++ src/components/Card/BookCard.js
@@ -1,28 +1,61 @@
 import React from 'react'
-import {Poster} from './Poster'
+import { Poster } from './Poster'
+import './Poster.css'
 function BookCard() {
 const [data,setData] = React.useState([])
-const[loading,setLoading] =React.useState(false)
-
-    React.useEffect(() => {
-        fetch("http://localhost:3001/bookInfo")
-          .then((data) => data.json())
-          .then((data) => {
+    const [loading, setLoading] = React.useState(false)
+    const [page, setPage] = React.useState(1);
+    const [search, setSearch] = React.useState('');
+ 
+    function callingFun() {
+          fetch(`http://localhost:3001/bookInfo?_page=${page}&_limit=3`)
+            .then((data) => data.json())
+            .then((data) => {
               console.log(data);
               setData(data);
               setLoading(false);
-          });
-    }, [])
+            });
+    }
+    React.useEffect(() => {
+        callingFun();
+    }, [page])
     console.log(data);
     return loading ? (
-        "Loading...."
+      "Loading...."
     ) : (
-      <div>
-            {data.map((items) => {
-                return (
-                    <Poster key={items.id} {...items}/>
-                )
-            })}
+      <div className="Parent">
+        <div>
+          <input
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+            type="text"
+            placeholder="search"
+          />
+        </div>
+        <div>
+          <button>Search</button>
+        </div>
+        {data.filter((val) => {})}
+        {data.map((items) => {
+          return <Poster key={items.id} {...items} />;
+        })}
+        <div className='btn'>
+          <button
+            onClick={() => {
+              setPage(page - 1);
+            }}
+          >
+            PrevPage
+          </button>
+          <button
+            onClick={() => {
+              setPage(page + 1);
+            }}
+          >
+            NextPage
+          </button>
+        </div>
       </div>
     );
 }
--- src/components/Card/Poster.js
+++ src/components/Card/Poster.js
@@ -1,10 +1,11 @@
 import './Poster.css'
 
 
-function Poster({title,rating,author,description,id}) {
-    
+function Poster({title,rating,author,description,id,image}) {
+ 
     return (
-        <div className = "Card">
+        <div className="Card">
+            <img src={image} alt="book" />
             <h1>{title}</h1>
             <h3>{rating}</h3>
             <h2>{author}</h2>
